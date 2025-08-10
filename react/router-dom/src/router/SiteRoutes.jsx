b122c288327cac4927bde562cdd5a56abd5cd64b
Buraktalhaa <talha45879@gmail.com>
2025-08-10 18:15:56 +0300

    feat: add site routing with React Router

--- react/router-dom/src/router/SiteRoutes.jsx
+++ react/router-dom/src/router/SiteRoutes.jsx
@@ -3,6 +3,8 @@ import Home from "../pages/Home"
 import Communication from "../pages/Communication"
 import About from "../pages/About"
 import PageNotFound from "../pages/PageNotFound"
+import { Category, Product, ProductsLayout } from "../products"
+import { Products } from "../products"
 
 
 export default function SiteRoutes() {
@@ -11,6 +13,11 @@ export default function SiteRoutes() {
             <Route path="/" element={<Home />}></Route>
             <Route path="/about-us" element={<About />}></Route>
             <Route path="/communication" element={<Communication />}></Route>
+            <Route path="/products" element={<ProductsLayout />}>
+                <Route index={true} element={<Products />}></Route>
+                <Route path="category/:categoryName" element={<Category />}></Route>
+                <Route path="product/:productId" element={<Product />}></Route>
+            </Route>
             <Route path="*" element={<PageNotFound />}></Route>
         </Routes>
     </>)
