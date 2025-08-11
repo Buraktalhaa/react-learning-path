abe31dc880585d55d01e8351c9d3c3e9115c5b41
Buraktalhaa <talha45879@gmail.com>
2025-08-11 12:40:08 +0300

    category added

---
+++ react/router-dom/src/products/Category.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import ProductCard from "./ProductCard"
+
+export default function Category() {
+    // const params = useParams()
+    // console.log(params);
+    // params categoryName i veriyor. Url den dolayı
+    const { categoryName } = useParams()
+
+    const [categoryProducts, setCategoryProducts] = useState([])
+
+    useEffect(() => {
+        fetch(`https://fakestoreapi.com/products/category/${categoryName}`)
+            .then(res => res.json())
+            .then(res => setCategoryProducts(res))
+            .catch(err => console.log(err));
+    }, [categoryName])
+
+    return (
+        <>
+            <h1>Category: {categoryName.toUpperCase()}</h1>
+            <div className="row row-cols-sm-3 row-cols-md-4">
+                {categoryProducts.map(item => <ProductCard key={item.id} item={item} />)}
+            </div>
+        </>
+    )
+}
