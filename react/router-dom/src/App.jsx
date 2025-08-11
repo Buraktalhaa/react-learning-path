70ea48473d17d8912cf6dcb8136568f48e23efb9
Buraktalhaa <talha45879@gmail.com>
2025-08-11 12:37:44 +0300

    Add user login/logout handlers with localStorage persistence

--- react/router-dom/src/App.jsx
+++ react/router-dom/src/App.jsx
@@ -1,15 +1,41 @@
+import { useEffect, useState } from "react"
 import Navbar from "./components/navbar"
 import SiteRoutes from "./router/SiteRoutes"
+import { useNavigate } from "react-router-dom"
 
 function App() {
+  const navigate = useNavigate()
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true) 
+
+  useEffect(() => {
+    const savedUser = localStorage.getItem("user")
+    if(savedUser){
+      setUser(JSON.parse(savedUser))
+    }
+    setLoading(false)
+  }, [])
+
+  function handleLogIn() {
+    const userData = {id: 1, name: "lorem"}
+    setUser(userData)
+    localStorage.setItem("user", JSON.stringify(userData))
+    navigate('/')
+  }
+
+  function handleLogout() {
+    localStorage.removeItem("user")
+    setUser(null)
+  }
+
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar user={user} handleLogout={handleLogout}></Navbar>
       <div className="container">
         <div className="row">
           <div className="col-sm-12">
-            
-          <SiteRoutes></SiteRoutes>
+
+            <SiteRoutes handleLogIn={handleLogIn} user={user} loading={loading}></SiteRoutes>
 
           </div>
         </div>
