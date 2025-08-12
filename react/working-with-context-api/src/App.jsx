731072b96cb32b84ca962a539e2565d756fff922
Buraktalhaa <talha45879@gmail.com>
2025-08-12 18:12:51 +0300

    started to new learning project

---
+++ react/working-with-context-api/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Navbar></Navbar>
+      <div className="container">
+        <div className="row">
+          <h1>App</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
