8f134049ed1fe630923a26460fa1527c48e6a47d
Buraktalhaa <talha45879@gmail.com>
2025-08-14 15:34:52 +0300

    Routes added

--- react/working-with-context-api/src/App.jsx
+++ react/working-with-context-api/src/App.jsx
@@ -1,17 +1,29 @@
 import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Navbar></Navbar>
-      <div className="container">
+      <Navbar></Navbar>
+      <div className="container my-3">
         <div className="row">
-          <h1>App</h1>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/about' element={<About />}></Route>
+            <Route path='/contact' element={<Contact />}></Route>
+          </Routes>
         </div>
       </div>
+      <Footer></Footer>
     </>
   )
 }
