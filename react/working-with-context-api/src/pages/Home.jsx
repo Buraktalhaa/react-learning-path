57a6e7851fa5b8196b2af3ada86b8980954dfa6f
Buraktalhaa <talha45879@gmail.com>
2025-08-14 15:35:33 +0300

    Update Home component to display multiple articles with structured content

--- react/working-with-context-api/src/pages/Home.jsx
+++ react/working-with-context-api/src/pages/Home.jsx
@@ -1,7 +1,87 @@
 export default function Home() {
     return (
         <>
-            <h1>Home</h1>
+            <div className="col-sm-12">
+                <h1>Main Page</h1>
+                <hr />
+                {/* article*4>h2>{Article $}^p*4>lorem20^ul>li*10>lorem4 */}
+                <article>
+                    <h2>Article 1</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque libero officiis corrupti! Officia optio autem ab, necessitatibus illo distinctio quae.</p>
+                    <p>Libero adipisci at sint error praesentium accusamus, unde nesciunt dolorum fugiat consectetur. Voluptatibus, fugit! Praesentium fugit natus dolores repudiandae illum?</p>
+                    <p>Totam distinctio, saepe perferendis possimus pariatur suscipit, quasi ab cumque, a quae tempora ad neque? Autem illo voluptates vel fuga.</p>
+                    <p>Repellendus dolore dolorem quia voluptatem odit obcaecati laboriosam iste delectus assumenda nam ab nostrum vero, alias rerum quis, impedit soluta.</p>
+                    <ul>
+                        <li>Lorem ipsum dolor sit.</li>
+                        <li>Reiciendis similique ratione iure?</li>
+                        <li>Tempora quia ipsam explicabo?</li>
+                        <li>Incidunt dicta consequatur a?</li>
+                        <li>Voluptatem reprehenderit quisquam ex.</li>
+                        <li>Eaque, quod sed! Velit!</li>
+                        <li>Dolor exercitationem reprehenderit ipsa.</li>
+                        <li>Quae veniam non quaerat.</li>
+                        <li>Ullam nesciunt repellat nihil!</li>
+                        <li>Nisi nam commodi nihil.</li>
+                    </ul>
+                </article>
+                <article>
+                    <h2>Article 2</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit debitis sit commodi laudantium laboriosam optio atque molestias quas dolore corrupti?</p>
+                    <p>Laudantium fuga quam quasi non provident deserunt itaque, consectetur assumenda adipisci excepturi officia iusto voluptates aliquam omnis dignissimos mollitia molestias.</p>
+                    <p>Fugit, omnis dolore aliquam quis repudiandae eaque earum nam quisquam fugiat vero sapiente neque? Rem distinctio molestias neque iure similique?</p>
+                    <p>Deleniti nesciunt aperiam asperiores, repellat sapiente aspernatur dicta necessitatibus debitis odit mollitia quisquam at placeat rerum tempore. Aliquam, esse soluta?</p>
+                    <ul>
+                        <li>Lorem ipsum dolor sit.</li>
+                        <li>Illum in modi error?</li>
+                        <li>Velit nisi possimus molestiae?</li>
+                        <li>Possimus cumque iste perferendis.</li>
+                        <li>Ducimus excepturi dolores asperiores?</li>
+                        <li>Nisi earum doloremque accusantium?</li>
+                        <li>Iste incidunt pariatur quam!</li>
+                        <li>At quam similique illo.</li>
+                        <li>Voluptates, nostrum? Sunt, voluptatibus.</li>
+                        <li>Itaque, dolore. Ipsum, tempore!</li>
+                    </ul>
+                </article>
+                <article>
+                    <h2>Article 3</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi laudantium eveniet odio laborum minus totam distinctio id, illum earum ratione.</p>
+                    <p>Quasi porro, corrupti, dignissimos cupiditate possimus cum culpa alias quos facere harum optio! Animi voluptates excepturi obcaecati eveniet non dignissimos?</p>
+                    <p>Odit quidem natus aperiam tenetur, adipisci nulla culpa fugit vero quis reiciendis optio quisquam quia minima! Harum temporibus voluptates excepturi.</p>
+                    <p>Excepturi fugiat quia inventore maiores ea a, recusandae molestiae suscipit omnis illo ut expedita, soluta officia rerum tenetur explicabo dignissimos.</p>
+                    <ul>
+                        <li>Lorem ipsum dolor sit.</li>
+                        <li>Officia, obcaecati? Exercitationem, nemo!</li>
+                        <li>Reiciendis nostrum harum numquam!</li>
+                        <li>Sequi fugit ducimus quisquam.</li>
+                        <li>Nemo harum adipisci nihil?</li>
+                        <li>Quaerat beatae nihil quos.</li>
+                        <li>Ut possimus alias fuga?</li>
+                        <li>Voluptatibus fugiat dicta tenetur.</li>
+                        <li>Magni tempore sapiente amet!</li>
+                        <li>Rem quis nesciunt ducimus.</li>
+                    </ul>
+                </article>
+                <article>
+                    <h2>Article 4</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio molestias temporibus deleniti nulla! Quia libero ab aliquid fugiat? Ipsa, tenetur.</p>
+                    <p>Dolores, cupiditate nemo tenetur dolorum consequatur iure fuga maxime blanditiis non, impedit debitis. Quaerat beatae quos iusto, quibusdam explicabo non.</p>
+                    <p>Cum cumque voluptatibus suscipit eaque, delectus quos magnam? Ipsum iste unde quia. Illo, facere reiciendis quam error harum provident molestiae.</p>
+                    <p>Ipsum qui, aspernatur aperiam numquam esse harum voluptate vero repellendus explicabo, a excepturi odio tempora sit tempore iste ratione fuga?</p>
+                    <ul>
+                        <li>Lorem ipsum dolor sit.</li>
+                        <li>Eveniet hic tempore est?</li>
+                        <li>Doloremque, iusto quasi! Harum!</li>
+                        <li>Adipisci ipsam pariatur maxime?</li>
+                        <li>Nobis voluptatibus beatae maiores!</li>
+                        <li>Quibusdam fugit molestias quas?</li>
+                        <li>Sunt hic mollitia ipsum!</li>
+                        <li>Autem modi deleniti neque?</li>
+                        <li>Itaque enim illo fugiat.</li>
+                        <li>Perferendis dolorem sunt illum.</li>
+                    </ul>
+                </article>
+            </div>
         </>
     )
 }
