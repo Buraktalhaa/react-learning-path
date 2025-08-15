37a8818dd3f618e89f288291e9a3532e5e94ba84
Buraktalhaa <talha45879@gmail.com>
2025-08-15 09:36:16 +0300

    Refactor routing by creating SiteRoutes component for better structure

--- react/working-with-context-api/src/App.jsx
+++ react/working-with-context-api/src/App.jsx
@@ -1,12 +1,8 @@
 import { useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
 
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,11 +12,7 @@ function App() {
       <Navbar></Navbar>
       <div className="container my-3">
         <div className="row">
-          <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/about' element={<About />}></Route>
-            <Route path='/contact' element={<Contact />}></Route>
-          </Routes>
+          <SiteRoutes />
         </div>
       </div>
       <Footer></Footer>
---
+++ react/working-with-context-api/src/routes/SiteRoutes.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+
+export default function SiteRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<Contact />} />
+    </Routes>
+  );
+}
