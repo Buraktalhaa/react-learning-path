4e562f4661215921e4a86d61adc4f84fd6b650b0
Buraktalhaa <talha45879@gmail.com>
2025-08-21 16:21:10 +0300

    refactor: Remove handleLogIn prop from Login component; use SiteContext for login handling

--- react/projects/router-dom/src/pages/Login.jsx
+++ react/projects/router-dom/src/pages/Login.jsx
@@ -1,4 +1,8 @@
-export default function Login({ handleLogIn }) {
+import { useContext } from "react"
+import { SiteContext } from "../context/SiteContext"
+
+export default function Login() {
+    const { handleLogIn } = useContext(SiteContext)
     return (<>
         <h1>Login</h1>
         <button
